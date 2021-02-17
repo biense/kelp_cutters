954a59f617a7257a4ac987dd589da7ff5ea2d5a5
Lennard <mac@Macs-MacBook-Pro.local>
2021-02-17 18:23:10 +0100

    adds articles controller/index and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # routes get request for articles to articles controller index
+  get "/articles", to: "articles#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
