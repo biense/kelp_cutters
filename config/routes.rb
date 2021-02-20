624b9bbf9e92306f50bc6ea7203e5729bd1e1f21
Lennard <mac@Macs-MacBook-Pro.local>
2021-02-20 14:33:20 +0100

    adds resources routing and implements link_to helper

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # routes articles/index as the main page or "root"
   root "articles#index"
   
-  # routes get request for articles to articles controller index
-  get "/articles", to: "articles#index"
+  # takes care of all CRUD routes for articles
+  resources :articles
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
