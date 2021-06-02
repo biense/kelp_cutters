93faedc801f18869bcfcd418dd43c396b87d9c87
Lennard <mac@Macs-MacBook-Pro.local>
2021-06-02 16:04:53 +0200

    adds user validation for name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+  
+  validates :name, presence: false, uniqueness: false, format: { with: /([A-Za-z ])\w/, message: "Name contains character(s) that is (or are) not allowed" }
 end
