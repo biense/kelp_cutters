1c10d35fc0a29ca41fcef29ba5095a23be9afd96
Lennard <mac@Macs-MacBook-Pro.local>
2021-06-02 12:50:39 +0200

    changes article validation regex

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+    validates :title, presence: true, format: { with: /[a-zA-Z0-9_]+( [a-zA-Z0-9_]+)/, message: "Title contains character(s) that is (or are) not allowed" }
     validates :body, presence: true, length: { minimum: 10 }
 end
