922ddbc5d5e5d6072a9dc18a259289e7da328ee6
Lennard <mac@Macs-MacBook-Pro.local>
2021-06-02 10:55:52 +0200

    adds validation and validation error message for article title

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
+    validates :title, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
     validates :body, presence: true, length: { minimum: 10 }
 end
